2beded422df0d41fa6d2e7e7e13e6041ddc259b5
Tong ZHAN <zhantong1994@163.com>
2017-01-02 22:37:45 +0800

    解决获取环境信息失效的问题
    由于北京市环保监测中心网站改版，之前的API不能使用，更新抓取数据方式。

--- get_environment.py
+++ get_environment.py
@@ -1,13 +1,20 @@
 import urllib.request
 import json
+import re
+import base64
 
 
 def get_real_time_environment():
-    url = 'http://zx.bjmemc.com.cn/web/Service.ashx'
+    url = 'http://zx.bjmemc.com.cn/index.shtml'
     content = None
     with urllib.request.urlopen(url) as f:
         content = f.read().decode('utf-8')
-        content = json.loads(content)
+    now_data_var_name = re.findall(r'var\snowdata.*?\+(.*?)\+', content)[0]
+    content_base64_encode = re.findall(
+        r'var\s%s.*?decode\((.*?)\)' % now_data_var_name, content)[0]
+    content = base64.b64decode(content_base64_encode).decode('utf-8')
+    content = json.loads(content)
+
     result = {
         'time': content['Table'][0]['Date_Time'],
         'station': content['Table'][0]['station'],
